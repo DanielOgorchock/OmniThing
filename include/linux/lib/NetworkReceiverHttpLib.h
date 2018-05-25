18313b421333595114765dfd89cce403e242b65c
Daniel J. Ogorchock <djogorchock@gmail.com>
2018-05-25 14:34:13 -0400

    working on impl

--- include/linux/lib/NetworkReceiverHttpLib.h
+++ include/linux/lib/NetworkReceiverHttpLib.h
@@ -2,12 +2,26 @@
 #define OMNI_NETWORKRECEIVERHTTPLIB_H
 
 #include "NetworkReceiver.h"
+#include "httplib.h"
 
 namespace omni
 {
     class NetworkReceiverHttpLib : public NetworkReceiver
     {
+        private:
+            httplib::Server m_Server;
+            char m_JsonBuffer[128] //TODO: make size configurable
 
-    }
+        protected:
+
+        public:
+            NetworkReceiverHttpLib();
+            virtual ~NetworkReceiverHttpLib();
+
+            virtual void init();
+            virtual void run();
+            virtual const char* getJsonString();
+
+    };
 }
 #endif
--- src/linux/lib/NetworkReceiverHttpLib.cpp
+++ src/linux/lib/NetworkReceiverHttpLib.cpp
@@ -5,4 +5,32 @@ namespace omni
 //private
 //protected
 //public
+    NetworkReceiverHttpLib::NetworkReceiverHttpLib():
+        m_Server()
+    {
+        m_JsonBuffer[0] = 0; 
+    }
+
+    NetworkReceiverHttpLib::~NetworkReceiverHttpLib()
+    {
+
+    }
+
+    void NetworkReceiverHttpLib::init()
+    {
+        m_Server.Post("/", [](const auto& req, auto& res)
+        {
+            res.set_content("ok", "text/plain");
+        });
+    }
+
+    void NetworkReceiverHttpLib::run()
+    {
+
+    }
+
+    const char* NetworkReceiverHttpLib::getJsonString()
+    {
+        return nullptr;
+    }
 }
