f9b0e296c862346db78f8da7b7a8809bc38bc9cc
Daniel J. Ogorchock <djogorchock@gmail.com>
2018-05-25 02:17:42 -0400

    added a stub implementation of digitalinputpin for tests

---
+++ include/core/DigitalInputPinStub.h
@@ -0,0 +1,22 @@
+#ifndef OMNI_DIGITALINPUTPINSTUB_H
+#define OMNI_DIGITALINPUTPINSTUB_H
+
+#include "DigitalInputPin.h"
+
+namespace omni
+{
+    class DigitalInputPinStub : public DigitalInputPin
+    {
+        private:
+
+        protected:
+            bool readPin() final;
+
+        public:
+            DigitalInputPinStub(unsigned char pin, bool invertLogic);
+
+            virtual ~DigitalInputPinStub();
+    };
+}
+
+#endif
---
+++ src/core/DigitalInputPinStub.cpp
@@ -0,0 +1,28 @@
+#include "DigitalInputPinStub.h"
+
+// TODO: replace this with a LOGGER class
+#include <iostream>
+
+namespace omni
+{
+//private
+
+
+//protected
+    bool DigitalInputPinStub::readPin()
+    {
+        static bool bogusVal = false;
+        
+        bogusVal = !bogusVal
+        std::cout << "DigitalInputPinStub: readPin(): " << bogusVal << std::endl; 
+        return bogusVal;
+    }
+
+//public
+    DigitalInputPinStub::DigitalInputPinStub(unsigned char pin, bool invertLogic):
+        DigitalInputPin(pin, invertLogic)
+    {
+        std::cout << "DigitalInputPinStub: Constructor(): pin=" << pin << "  invertLogic=" << invertLogic << std::endl;
+    }
+
+}
