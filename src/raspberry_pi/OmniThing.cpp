955314977fb50d93d8dd593315eb3666e854615d
Daniel J. Ogorchock <djogorchock@gmail.com>
2018-05-25 03:39:08 -0400

    added main function for raspberry pi

---
+++ src/raspberry_pi/OmniThing.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <pigpio.h>
+
+#include "DigitalInputPinRaspberryPi.h"
+#include "DigitalOutputPinRaspberryPi.h"
+
+
+int main(int argc, char* argv[])
+{
+    if(gpioInitialize() == PI_INIT_FAILED)
+    {
+        std::cerr << "Failed to initialize pigpio library" << std::endl;
+    }
+
+    return 0;
+}
