5b1bc143a50e444f0ef90ad958dad05423946332
Daniel J. Ogorchock <djogorchock@gmail.com>
2018-05-25 11:47:34 +0000

    edited test main

--- src/raspberry_pi/OmniThing.cpp
+++ src/raspberry_pi/OmniThing.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <pigpio.h>
 #include <frozen.h>
+#include <unistd.h>
 
 #include "DigitalInputPinRaspberryPi.h"
 #include "DigitalOutputPinRaspberryPi.h"
@@ -8,9 +9,21 @@
 
 int main(int argc, char* argv[])
 {
+    using namespace omni;
+
     if(gpioInitialise() == PI_INIT_FAILED)
     {
         std::cerr << "Failed to initialize pigpio library" << std::endl;
+    }
+    std::cout << "Initialized pigpio\n";
+
+    DigitalOutputPinRaspberryPi out(22, false, false);
+
+    while(true)
+    {
+        sleep(1);
+        std::cout << "Toggling pin\n";
+        out.writeVoid(); 
     }
 
     return 0;
