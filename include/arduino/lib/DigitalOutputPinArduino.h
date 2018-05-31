b29820fd06457c9da3f8fa7571428ec01bf1fc65
Daniel J. Ogorchock <djogorchock@gmail.com>
2018-05-31 15:23:17 -0400

    added Digital Output Pin for arduino

---
+++ include/arduino/lib/DigitalOutputPinArduino.h
@@ -0,0 +1,29 @@
+#ifndef OMNI_DIGITALOUTPUTPINARDUINO_H
+#define OMNI_DIGITALOUTPUTPINARDUINO_H
+
+#include "DigitalOutputPin.h"
+
+namespace omni
+{
+    class DigitalOutputPinArduino : public DigitalOutputPin
+    {
+        private:
+            static DigitalOutputPinArduino* createFromJson(const char* json);
+
+        protected:
+            void writePin(bool b) final;
+
+        public:
+            DigitalOutputPinArduino(unsigned short pin, bool initialVal, bool invertLogic);
+
+            virtual ~DigitalOutputPinArduino();
+
+            static OutputVoid* createVoidFromJson(const char* json);
+            static OutputBool* createBoolFromJson(const char* json);
+
+            static const char* Type;
+            static ObjectConfig<OutputVoid> OutputVoidConf;
+            static ObjectConfig<OutputBool> OutputBoolConf;
+    };
+}
+#endif
---
+++ src/arduino/lib/DigitalOutputPinArduino.cpp
@@ -0,0 +1,60 @@
+#include "DigitalOutputPinArduino.h"
+
+#include <Arduino.h>
+#include <string.h>
+#include "frozen.h"
+
+namespace omni
+{
+//private
+    DigitalOutputPinArduino* DigitalOutputPinArduino::createFromJson(const char* json)
+    {
+        unsigned int len = strlen(json);
+
+        unsigned int pin;
+        bool initial;
+        bool invert;
+
+        if(json_scanf(json, len, "{pin: %u, initial: %B, invert: %B}", &pin, &initial, &invert) != 3)
+        {
+            return nullptr;
+        }
+
+        return new DigitalOutputPinArduino(pin, initial, invert);
+    }
+
+//protected
+    void DigitalOutputPinArduino::writePin(bool b)
+    {
+        digitalWrite(getPin(), b);
+    }
+
+//public
+    DigitalOutputPinArduino::DigitalOutputPinArduino(unsigned short pin, bool initialVal, bool invertLogic):
+        DigitalOutputPin(pin, initialVal, invertLogic)
+    {
+        pinMode(pin, OUTPUT);
+
+        writeBool(initialVal);
+    }
+
+    DigitalOutputPinArduino::~DigitalOutputPinArduino()
+    {
+
+    }
+
+    OutputVoid* DigitalOutputPinArduino::createVoidFromJson(const char* json)
+    {
+        return createFromJson(json);
+    }
+
+    OutputBool* DigitalOutputPinArduino::createBoolFromJson(const char* json)
+    {
+        return createFromJson(json);
+    }
+
+    const char* DigitalOutputPinArduino::Type = "DigitalOutputPinArduino";
+    ObjectConfig<OutputVoid> DigitalOutputPinArduino::OutputVoidConf(Type, createVoidFromJson);
+    ObjectConfig<OutputBool> DigitalOutputPinArduino::OutputBoolConf(Type, createBoolFromJson);
+
+}
