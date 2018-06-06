5ac52148e1292dab0c4377ba39ff8c28d26a7043
Daniel J. Ogorchock <djogorchock@gmail.com>
2018-06-06 04:32:43 -0400

    added CommandExecuter

---
+++ include/os_common/lib/CommandExecuter.h
@@ -0,0 +1,31 @@
+#ifndef OMNI_COMMANDEXECUTER_H
+#define OMNI_COMMANDEXECUTER_H
+
+#include "OutputVoid.h"
+#include "ObjectConfig.h"
+
+namespace omni
+{
+    class CommandExecuter : public OutputVoid
+    {
+        private:
+            const char* m_CommandStr;
+            bool m_bMultithreaded;
+
+            void executeCommand();
+        protected:
+        public:
+            CommandExecuter(const char* command, bool multithread = false);
+            virtual ~CommandExecuter();
+
+            void writeVoid();
+
+            static OutputVoid* createFromJson(const char* json);
+
+            
+            static const char* Type;
+            static ObjectConfig<OutputVoid> OutputVoidConf;
+
+    };
+}
+#endif
---
+++ src/os_common/lib/CommandExecuter.cpp
@@ -0,0 +1,75 @@
+#include "CommandExecuter.h"
+
+#include "OmniThing.h"
+#include "Logger.h"
+
+#include <string.h>
+#include "frozen.h"
+#include <thread.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+namespace omni
+{
+//private
+    void CommandExecuter::executeCommand()
+    {
+        LOG << F("Executing: ") << m_CommandStr << Logger::endl;
+        if(system(nullptr))
+        {
+            LOG << F("ERROR: This platform does not support the system() function\n");
+        }
+
+        system(m_CommandStr);
+    }
+    
+//protected
+//public
+    CommandExecuter::CommandExecuter(const char* command, bool multithread):
+        m_CommandStr(command),
+        m_bMultithreaded(multithread)
+    {
+
+    }
+
+    CommandExecuter::~CommandExecuter()
+    {
+
+    }
+
+    void CommandExecuter::writeVoid()
+    {
+        if(m_bMultithreaded)
+        {
+            std::thread commandRunner(&executeCommand, this);
+            commandRunner.detach();
+        }
+        else
+        {
+            executeCommand();
+        }
+    }
+
+    OutputVoid* CommandExecuter::createFromJson(const char* json)
+    {
+        unsigned int len = strlen(json);
+        char* command;
+        bool multithread;
+
+        if(json_scanf(json, len, "{command: %Q}", &command) != 1)
+        {
+            return nullptr;
+        }
+
+        if(json_scanf(json, len, "{multithread: %B}", &multithread) != 1)
+        {
+            multithread = false;
+        }
+
+        return new CommandExecuter(command, multithread);
+    }
+
+    const char* CommandExecuter::Type = "CommandExecuter";
+    ObjectConfig<OutputVoid> CommandExecuter::OutputVoidConf(Type, createFromJson);
+}
