0f1601b04eee39d0ffea290a0d7b7e8da4627612
Daniel J. Ogorchock <djogorchock@gmail.com>
2018-08-18 14:16:32 -0500

    allow json config import

--- webpage/files/configuration.js
+++ webpage/files/configuration.js
@@ -1,1 +1,73 @@
-console.log("TEST")
+var blankConfiguration = {};
+var configuration = {};
+
+
+var configAltered = false;
+
+$(window).on('load', function(){
+    $.getJSON("blank_config.json", function(data){
+        blankConfiguration = data;
+        resetConfig();
+        configAltered = false;
+    });
+
+    $("#buttonResetJson").click(resetConfig);
+
+    $("#buttonImportJson").click(loadConfig);
+
+    $(window).bind('beforeunload', function(){
+        if(configAltered)
+        {
+            return true;
+        }
+        return undefined;
+    });
+});
+
+var resetConfig = function(){
+    configuration = Object.assign(blankConfiguration); 
+    updateRawConfig();
+}
+
+var updateRawConfig = function(){
+    configAltered = true;
+    $("#raw_json").text(JSON.stringify(configuration, null, 4));
+}
+
+var loadConfig = function(){
+    var input, file, fr;
+
+    if (typeof window.FileReader !== 'function') {
+        alert("The file API isn't supported on this browser yet.");
+        return;
+    }
+
+    input = document.getElementById('fileImportJson');
+    if (!input) {
+        alert("Um, couldn't find the fileinput element.");
+    }
+    else if (!input.files) {
+        alert("This browser doesn't seem to support the `files` property of file inputs.");
+    }
+    else if (!input.files[0]) {
+        alert("No file has been selected");
+    }
+    else {
+        file = input.files[0];
+        fr = new FileReader();
+        fr.onload = receivedText;
+        fr.readAsText(file);
+    }
+
+    function receivedText(e) {
+        try{
+            let lines = e.target.result;
+            var tmp = JSON.parse(lines); 
+            configuration = tmp;
+        }
+        catch(err){
+            alert("Failed to parse json configuration. Are you sure you selected a json file?");
+        }
+        updateRawConfig();
+    }   
+}
