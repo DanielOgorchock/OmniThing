7dfab03960ba71151e209fb8ff713fe961b26ad7
Daniel J. Ogorchock <djogorchock@gmail.com>
2018-12-14 22:40:10 -0600

    use high res clock for micros

--- src/os_common/lib/OmniUtil.cpp
+++ src/os_common/lib/OmniUtil.cpp
@@ -26,7 +26,7 @@ namespace omni
 
     unsigned long long getMicros()
     {
-        return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
     }
 
     void sleepMicros(unsigned long long us)
