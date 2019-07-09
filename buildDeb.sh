ba42371ce95658440c7d12456bfbd5a86f6ad4ee
Daniel J. Ogorchock <djogorchock@gmail.com>
2019-07-08 19:42:23 -0500

    generate Release file when building deb

--- buildDeb.sh
+++ buildDeb.sh
@@ -46,4 +46,6 @@ cd ../..
 
 fakeroot dpkg-deb --build omnithing || exit
 
-dpkg-scanpackages ./ /dev/null | gzip -9c > Packages.gz
+dpkg-scanpackages ./ /dev/null | gzip -9c > Packages.gz &&
+apt-ftparchive release . > Release
+
